1c96ba05bf70d6dcbc29c60c5a9c48e6c7e7949b
Muhammad Saad Khalid <szsaadiii0@gmail.com>
2025-07-04 16:13:56 +0500

    04: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { Client } from "./client";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "Saad" }));
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div>
+      <Button
+        className="p-4 max-w-7xl mx-auto"
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "Saad" })}
+      >
+        Invoke
+      </Button>
+    </div>
   );
 };
 
